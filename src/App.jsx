4b9ba929de79aeee2ebe6ada92362adb991c6b67
rmusayevr <rmusayevr19@gmail.com>
2023-09-17 16:18:25 +0400

    lorem ipsum

---
+++ src/App.jsx
@@ -0,0 +1,44 @@
+import { useState } from "react";
+import data from "./data";
+import { nanoid } from 'nanoid'
+
+const App = () => {
+  const [count, setCount] = useState(1);
+  const [text, setText] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    let amount = parseInt(count);
+    setText(data.slice(0, amount));
+  };
+
+  return (
+    <section className="section-center">
+      <h4>Tired of boring lorem ipsum?</h4>
+      <form className="lorem-form" onSubmit={handleSubmit}>
+        <div className="form-row">
+          <label htmlFor="amount">paragraphs:</label>
+          <input
+            type="number"
+            name="amount"
+            id="amount"
+            min="1"
+            step="1"
+            max="8"
+            value={count}
+            onChange={(e) => setCount(e.target.value)}
+          />
+          <button type="submit" className="btn">
+            Add Paragraph
+          </button>
+        </div>
+      </form>
+      <article className="lorem-text">
+        {text.map((item)=>{
+          return <p key={nanoid()}>{item}</p>
+        })}
+      </article>
+    </section>
+  );
+};
+export default App;
